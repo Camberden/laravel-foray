243e230c4bdd3beae18f19ef28a44051484c8599
Camberden <42500247+Camberden@users.noreply.github.com>
2024-08-25 22:59:32 -0400

    Restyled page; regained connectivity; database working and ready for testing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,31 +2,41 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<link rel="shortcut icon" href="{{ asset('/image-crystal-chrispy-favicon.png') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/app.css') }}" media="screen">
+	<link href="https://fonts.googleapis.com/css?family=Lora:400italic" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker:400" rel="stylesheet" type="text/css">
+	<title>Chrispy"s Site</title>
 </head>
 
 <body>
-	<h1>I am tired but also not! :D</h1>
+
+	
+	<h1><span id="camberden">Camberden's</span><span class="marker">Laravel Workspace<span></h1>
+
+	<main class="index-grid">
 
 	@auth
-	<div>
+	<div class="index-item">
 		<p>Congrats! You are logged in!</p>
 		<form action="/logout" method="POST">
 			@csrf
 			<button>Log out!</button>
 		</form>
+	</div>
 
-<div>
-	<h2>Create new post!</h2>
-	<form action="/create-post" method="POST">
+	<div class="index-item">
+		<form action="/create-post" method="POST">
 		@csrf
-		<input type="text" name="title" placeholder="Enter title of post">
-		<textarea name="body" placeholder="Content of your post"></textarea>
-		<button>Save Post!</button>
-		
-</form>	
+		<div class="sub-index-grid">
+			<div class="sub-index-item"><input type="text" name="title" placeholder="Enter title of post" id="post-title"></div>
+			<div class="sub-index-item"><textarea name="body" placeholder="Content of post" id="textarea-title"></textarea></div>
+			<div class="sub-index-item"><button>Save Post!</button></div>
+			</form>
+		</div>
+	</div>
 
-<div>
+	<div class="index-item">
 	<h2>All posts to date below!</h2>
 	@foreach($posts as $post)
 	<div>
@@ -34,15 +44,10 @@
 		<p>{{$post["body"]}}</p>
 	</div>
 	@endforeach
-</div>
-
-</div>
-
-
-
 	</div>
+	
 	@else
-	<div style="border: 3px solid black;">
+	<div style="border: 3px solid black;" class="index-item">
 		<h2>Register</h2>
 		<form action="/register" method="POST"><!-- THIS IS THE URL THE FORM SUBMITS TO -->
 			@csrf <!-- COOKIE PROTECTION -->
@@ -52,7 +57,7 @@
 			<button>Register</button>
 		</form>
 	</div>
-	<div style="border: 3px solid black;">
+	<div style="border: 3px solid black;" class="index-item">
 		<h2>Login</h2>
 		<form action="/login" method="POST"><!-- THIS IS THE URL THE FORM SUBMITS TO -->
 			@csrf <!-- COOKIE PROTECTION -->
@@ -63,6 +68,6 @@
 	</div>
 	@endauth
 	
-	
+	</main>
 </body>
 </html>
